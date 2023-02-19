16d29ed0f78d7d2fe29b1ed3333a1335a095c05c
olcayarpac <olcayarpac@yahoo.com>
2023-02-19 18:16:43 +0300

    crud operations added

--- StoreAPI/Controllers/ProductController.cs
+++ StoreAPI/Controllers/ProductController.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StoreAPI.Controllers;
@@ -6,13 +8,14 @@ namespace StoreAPI.Controllers;
 [Route("api/[controller]/")]
 public class ProductController : ControllerBase
 {
-    private static readonly Product[] Products = new[]
+    private static List<Product> Products = new List<Product>
     {
         new Product{Id = 1, Price = 25699, Name = "IPhone 12", Stock = 5 },
         new Product{Id = 2, Price = 36999, Name = "Macbook Air", Stock = 5 },
         new Product{Id = 3, Price = 42699, Name = "Macbook Pro", Stock = 5 },
         new Product{Id = 4, Price = 5999, Name = "Airpods", Stock = 5 },
     };
+    private int lastId = 5;
 
     private readonly ILogger<ProductController> _logger;
 
@@ -39,4 +42,77 @@ public class ProductController : ControllerBase
         return Products.Where(p => p.Id == id).ToArray();
     }
 
+    [HttpPost]
+    public IActionResult PostProduct([FromBody] Product product)
+    {
+        var existingProduct = Products.SingleOrDefault(p => p.Name == product.Name);
+        if (existingProduct is not null)
+        {
+            return Conflict("Product already exists");
+        }
+        product.Id = lastId;
+        this.lastId++;
+        Products.Add(product);
+        return Created(Request.Path.Value + "/" + product.Id, product);
+    }
+
+    [HttpPut]
+    public IActionResult PutProduct([FromBody] Product product)
+    {
+        var existingProduct = Products.SingleOrDefault(p => p.Id == product.Id);
+        if (existingProduct is null)
+        {
+            Products.Add(product);
+            lastId++;
+            return Created(Request.Path.Value + "/" + product.Id, product);
+        }
+        var index = Products.IndexOf(existingProduct);
+        Products[index] = product;
+        return Ok(existingProduct);
+    }
+
+    [HttpPatch]
+    public IActionResult PatchProduct([FromBody] Product product)
+    {
+        var existingProduct = Products.SingleOrDefault(p => p.Id == product.Id);
+        if (existingProduct is null)
+        {
+            return NotFound();
+        }
+
+        foreach (PropertyInfo prop in product.GetType().GetProperties())
+        {
+            var propValue = prop.GetValue(product, null);
+            if(propValue != null){
+                prop.SetValue(existingProduct, propValue);
+            }
+        }
+        return Ok(existingProduct);
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteProduct([FromBody] Product product)
+    {
+        var existingProduct = Products.SingleOrDefault(p => p.Id == product.Id);
+        if (existingProduct is null)
+        {
+            return NotFound();
+        }
+
+        Products.Remove(existingProduct);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProductById([FromBody] int id)
+    {
+        var existingProduct = Products.SingleOrDefault(p => p.Id == id);
+        if (existingProduct is null)
+        {
+            return NotFound();
+        }
+
+        Products.Remove(existingProduct);
+        return NoContent();
+    }
 }
--- StoreAPI/Product.cs
+++ StoreAPI/Product.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StoreAPI;
 
 public class Product
 {
-    public int Id {get; set;}
+    public int Id { get; set; }
+
     public double Price { get; set; }
 
-    public int Stock {get; set; }
+    public int? Stock { get; set; }
 
     public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
 }
